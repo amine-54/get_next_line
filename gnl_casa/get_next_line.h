1dac42267d99885b71fd0ebbc8f55475914dce50
amine-54 <m.manyani0@gmail.com>
2025-01-08 15:43:22 +0100

    .

---
+++ gnl_casa/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanyani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 11:15:05 by mmanyani          #+#    #+#             */
+/*   Updated: 2025/01/08 15:29:22 by rqf              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char    *actual_line(char *holder);
+char    *update_holder(char *holder);
+
+#endif
