096bc6acd67f7d462c73dc87bdd084e0d53deecd
amine-54 <m.manyani0@gmail.com>
2025-01-25 09:46:18 +0100

    .

--- main2.c
+++ main2.c
@@ -5,12 +5,20 @@ int main()
 {
 	char	*line;
 	int fd = open("get_next_line.h", O_RDWR);
+/*
+	line = get_next_line(fd);
+
+	printf("%s", line);
+	close(fd);
+*/
 	while ((line = get_next_line(fd)))
 	{
 		printf("%s", line);
 		free(line);
 	}
 	close(fd);
+	
+
 }
 
 
